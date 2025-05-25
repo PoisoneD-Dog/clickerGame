f80bc03fad9b6ffd3cefcce08d5c11d9a002f7c1
alexis <yakuzalchicha@gmail.com>
2025-05-24 19:11:15 -0500

    add/meetMountain

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const scoreDisplay = document.getElementById("scoreDisplay");
 document.getElementById("clickme").addEventListener("click", function () {
   score += powerUp1;
   scoreDisplay.textContent = score;
+  updateMeatSprite(score);
 });
 
 document.getElementById("powerUp1").addEventListener("click", function () {
@@ -14,6 +15,7 @@ document.getElementById("powerUp1").addEventListener("click", function () {
     score -= 10;
     powerUp1 += 1;
     scoreDisplay.textContent = score;
+    updateMeatSprite(score);
   }
 });
 
@@ -22,10 +24,28 @@ document.getElementById("autoPower").addEventListener("click", function () {
     score -= 50;
     autoPower += 1;
     scoreDisplay.textContent = score;
+    updateMeatSprite(score);
   }
 });
 
 setInterval(function () {
   score += autoPower;
   scoreDisplay.textContent = score;
+  updateMeatSprite(score);
 }, 1000);
+
+function updateMeatSprite(score) {
+  const meat = document.getElementById("meatMountain");
+
+  if (score >= 20) {
+    meat.src = "Assets/Enverioment/mountainMeat (4).png";
+  } else if (score >= 10) {
+    meat.src = "Assets/Enverioment/mountainMeat (3).png";
+  } else if (score >= 2) {
+    meat.src = "Assets/Enverioment/mountainMeat (2).png";
+  } else {
+    meat.src = "Assets/Enverioment/mountainMeat (1).png";
+  }
+}
+
+
